662c2eaaad196cf33c800e201e74a090c35ce401
elf Pavlik <elf-pavlik@hackers4peace.net>
2019-06-10 13:52:03 -0500

    update sent email

--- server.js
+++ server.js
@@ -17,13 +17,15 @@ let cache, stats
 
 function generateMail (email) {
   return {
-    from: config.smtp.auth.user,
+    from: `"Alice in Government" <${config.smtp.auth.user}>`,
     to: email,
-    subject: 'Congratulations on being part of this citizen vote on Climate Change',
+    subject: 'Your vote was registered successfully',
     text: `
+Congratulations on being part of this citizen vote on Climate Change
+
 If you wish to read more about Alice in Government and how to get involved please visit: https://aliceingovernment.com/info
 
-Twitter: https://twitter.com/vsclimatechange
+Twitter: https://twitter.com/aliceingov
 Medium: https://medium.com/blockchainvsclimatechange
 Estonian Address Kiriku 6, Tallinn, 10130, Estonia
     `
